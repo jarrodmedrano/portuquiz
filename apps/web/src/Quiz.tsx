4258bcfb50b75a89b2753e17a8de769d92519fa2
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-08-24 16:32:01 -0500

    add more questions

--- apps/web/src/Quiz.tsx
+++ apps/web/src/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Question } from './App';
 
 export interface Answer {
@@ -10,6 +10,7 @@ export interface Answer {
 const Quiz = ({ questions }: { questions: Question[] }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState<Answer | null>(null);
+  const [shuffledAnswers, setShuffled] = useState<Answer[]>([]);
 
   function handleAnswerClick(answer: Answer) {
     setSelectedAnswer(answer);
@@ -22,12 +23,22 @@ const Quiz = ({ questions }: { questions: Question[] }) => {
 
   const question = questions[currentQuestion];
 
+  useEffect(() => {
+    const answers = question.answers;
+    let shuffled = [...answers];
+    for (let i = answers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setShuffled(shuffled);
+  }, [question]);
+
   return (
     <div>
       <div className="font-bold text-lg mb-4 mt-4">{question.text}</div>
 
       <div className="space-y-2">
-        {question.answers.map((answer: Answer) => (
+        {shuffledAnswers.map((answer: Answer) => (
           <button
             key={answer.id}
             className={`border px-4 py-2 w-full 
